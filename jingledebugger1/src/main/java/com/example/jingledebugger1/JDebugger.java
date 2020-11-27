5fe7a9823925624afa6ab2db64c82f82a11cd93f
suman ahuja <suman.jingleinfotech@gmail.com>
2020-11-27 14:30:55 +0530

    2nd commit

--- jingledebugger1/src/main/java/com/example/jingledebugger1/JDebugger.java
+++ jingledebugger1/src/main/java/com/example/jingledebugger1/JDebugger.java
@@ -3,7 +3,7 @@ package com.example.jingledebugger1;
 import android.util.Log;
 
 public class JDebugger {
-    void d(String S,String S2)
+    public void d(String S,String S2)
     {
         Log.d(S,S2);
     }
