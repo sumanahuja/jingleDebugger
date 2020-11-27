1f54791ba2e1527d8ed22227c3e3fd135504bb56
suman ahuja <suman.jingleinfotech@gmail.com>
2020-11-27 20:59:19 +0530

    error mudule

---
+++ error-debugger/src/main/java/com/example/error_debugger/ErrorDebug.java
@@ -0,0 +1,12 @@
+package com.example.error_debugger;
+
+import android.util.Log;
+
+public class ErrorDebug {
+
+    public static final String TAG="My jingle library ";
+    public static void e(String S2)
+    {
+        Log.e(TAG,S2);
+    }
+}
